978eb009eeecdc8546d69ec60948cbe3e35cf217
Kirbey Aldair Garcia Hernández <kirbey.garcia@gobierno1.yucatan.gob.mx>
2022-04-26 13:58:58 -0500

    Primer commit

---
+++ src/main/java/com/aws/cfkirbey/CfkirbeyApplication.java
@@ -0,0 +1,13 @@
+package com.aws.cfkirbey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfkirbeyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CfkirbeyApplication.class, args);
+	}
+
+}
